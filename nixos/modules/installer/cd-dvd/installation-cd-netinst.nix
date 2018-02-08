cebae07bc6b4dc61401c82257c20e42eee69ca33
Michael Ekstrand <md@ekstrandom.net>
2018-02-07 21:07:35 -0700

    add vmw_pvscsi to initrd

--- nixos/modules/installer/cd-dvd/installation-cd-netinst.nix
+++ nixos/modules/installer/cd-dvd/installation-cd-netinst.nix
@@ -13,6 +13,8 @@
     tmux
   ];
 
+  boot.initrd.availableModules = [ "vmw_pvscsi" ];
+
   services.openssh.startWhenNeeded = true;
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAkQE9UzcF7hAEBSUoe5tR2W5/UdJXtNuxPTqCfNXVeCEDy2yXpv2tbQxKMNQGFFSMtelBG4hx1Xa2G6SMBSSx9Dh+PKbNMWT7SUXK4ynceV2yH3e3OswmXIUm5HN2KLZVBR0ucPy4DTs30HuP9sQ0GvXEdQss/kDuJeo3lFMPfErN7cj6RKJCR6cRwg4CbHQ3z4EQokgZQ1fe997qFSHCDmAmL7YaZYHJL42Re0dCjcp5Qq9t/Ia+DE3PJ1C0KA0zMYJKXsJXDg2kXGz0d7ZsDyrZWz8GARAJHiRUrEielHspp7HJIXWX/phHQdlmTXqn1cmlxQxAsWKQjR7JIiIB1Q== michael@hopper.elehack.net"
